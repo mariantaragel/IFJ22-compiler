a5d4bb479ba2d7932d4d1833da8078d925968cad
Marián Tarageľ <mariantaragel@gmail.com>
2022-10-12 22:04:49 +0200

    Implementation of nonterminal func_def

--- parser.c
+++ parser.c
@@ -2,7 +2,7 @@
  * @name parser.c
  * @brief Implementation of top-down parser
  * @authors Marián Tarageľ
- * @date 10.10.2022
+ * @date 12.10.2022
  */
 
 #include "parser.h"
@@ -10,37 +10,145 @@
 
 int program()
 {
-    int program = FALSE;
     if (php_start()) {
         if (program_body()) {
             if (php_end()) {
-                program = TRUE;
+                return SYNTAX_CORRECT;
             }
         }
     }
-    return program;
+    return SYNTAX_ERROR;
 }
 
 int php_start()
 {
-    int php_start = FALSE;
     for (int i = 0; i < 8; i++) {
         token_t *token = get_token();
-        // TODO: verify tokens '<?php' func_id '(' 'strict_types' '=' integer_literal ')' ';' (swtich-case?)
-        if (php_start == FALSE)
-            break;
+        // TODO: verify tokens '<?php' func_id '(' 'strict_types' '=' integer_literal ')' ';'
     }
-    return php_start;
+    return SYNTAX_CORRECT;
 }
 
 int program_body()
 {
-    // TODO
-    return 0;
+    func_def();
+    program_body();
+
+    stmt_list();
+    program_body();
+
+    return SYNTAX_CORRECT;
 }
 
 int php_end()
 {
-    // TODO
-    return 0;
+    token_t *token = get_token();
+    // TODO: verify '?>'
+    return SYNTAX_CORRECT;
+}
+
+int func_def()
+{
+    token_t *token;
+    for (int i = 0; i < 3; i++) {
+        token = get_token();
+        switch (i) {
+        case 0:
+            if (token->type != FUNCTION) {
+                return SYNTAX_ERROR;
+            } break;
+        case 1:
+            if (token->type != FUNC_ID) {
+                return SYNTAX_ERROR;
+            } break;
+        case 2:
+            if (token->type != LB) {
+                return SYNTAX_ERROR;
+            } break;
+        default: break;
+        }
+    }
+
+    if (!param_list()) {
+        return SYNTAX_ERROR;
+    }
+
+    for (int i = 0; i < 2; i++) {
+        token = get_token();
+        if (!i) {
+            if (token->type != RB) {
+                return SYNTAX_ERROR;
+            }
+        }
+        else {
+            if (token->type != COLON) {
+                return SYNTAX_ERROR;
+            }
+        }
+    }
+
+    token = get_token();
+    if (!return_type(token)) {
+        return SYNTAX_ERROR;
+    }
+
+    token =  get_token();
+    if (token->type != LCB) {
+        return SYNTAX_ERROR;
+    }
+
+    if (!stmt_list()) {
+        return SYNTAX_ERROR;
+    }
+    
+    token =  get_token();
+    if (token->type != RCB) {
+        return SYNTAX_ERROR;
+    }
+
+    return SYNTAX_CORRECT;
+}
+
+int return_type(token_t *token)
+{
+    if (type(token)) {
+        return SYNTAX_CORRECT;
+    }
+    if (token->type != VOID_T) {
+        return SYNTAX_ERROR;
+    }
+    return SYNTAX_CORRECT;
+}
+
+int type(token_t *token)
+{
+    if (token->type == INT_T || token->type == NINT_T) {
+        return SYNTAX_CORRECT;
+    }
+    if (token->type == FLT_T || token->type == NFLT_T) {
+        return SYNTAX_CORRECT;
+    }
+    if (token->type == STR_T || token->type == NSTR_T) {
+        return SYNTAX_CORRECT;
+    }
+    return SYNTAX_ERROR;
+}
+
+int param_list()
+{
+    token_t *token = get_token();
+    if (!type(token)) {
+        return SYNTAX_ERROR;
+    }
+    
+    token = get_token();
+    if (token->type != VAR_ID) {
+        return SYNTAX_ERROR;
+    }
+
+    if (!param_next()) {
+        return SYNTAX_ERROR;
+    }
+    
+    return SYNTAX_CORRECT;
 }
--- parser.h
+++ parser.h
@@ -2,7 +2,7 @@
  * @name parser.h
  * @brief Interface of top-down parser
  * @authors Marián Tarageľ
- * @date 10.10.2022
+ * @date 12.10.2022
  */
 
 #ifndef _PARSER_H
@@ -12,6 +12,8 @@
 
 #define TRUE 1
 #define FALSE 0
+#define SYNTAX_CORRECT 1
+#define SYNTAX_ERROR 0
 
 int program();
 
@@ -20,5 +22,17 @@ int php_start();
 int program_body();
 
 int php_end();
+
+int func_def();
+
+int stmt_list();
+
+int param_list();
+
+int return_type(token_t *token);
+
+int type(token_t *token);
+
+int param_next();
 
 #endif
