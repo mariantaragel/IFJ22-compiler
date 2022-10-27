58f640fd2d45cb33b8b4f984c3a40a1ce878a85a
SuprisinglyBoring <david.klajbl@gmail.com>
2022-10-27 14:51:55 +0200

    Modified test to print AST

--- abstract_syntax_tree_test.c
+++ abstract_syntax_tree_test.c
@@ -1,11 +1,14 @@
 #include "abstract_syntax_tree.h"
-#include <stdlib.h>
+
+#include <stdio.h>
+
 
 int main(){
 	AST_node_t* root = AST_create_node(PROG_N);
 	if(root == NULL) return -1;
 
-	if(AST_add_child(root, AST_create_node(BODY_N)) != 0){
+	AST_node_t* new_node;
+	if((new_node = AST_create_add_child(root, FUNC_DEF_N)) == NULL){
 		AST_free(root);
 		return -1;
 	}
@@ -29,6 +32,6 @@ int main(){
 		AST_free(root);
 		return -1;
 	}
-	
+	AST_print(root, stdout);
 	AST_free(root);
 }
