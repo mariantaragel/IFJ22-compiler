bd0263e930e4891c0c902a49fa584b496b563fa0
SuprisinglyBoring <david.klajbl@gmail.com>
2022-10-31 18:59:18 +0100

    Added datatype type

--- symbol.h
+++ symbol.h
@@ -1,25 +1,38 @@
-#include <stdlib.h>  // size_t
+#include <stdlib.h>	 // size_t
 #include <stdbool.h> // bool
 #include "token.h"
 
 /**
  * @brief Type of symbol.
  */
-typedef enum {VAR_SYMBOL, FUNC_SYMBOL} symbol_type_t;
+typedef enum{
+	VAR_SYMBOL,
+	FUNC_SYMBOL
+} symbol_type_t;
+
+#ifndef DATATYPE_T
+#define DATATYPE_T
+/**
+ * @brief Datatype type.
+ * 	Its values are synonymous to FLT_T,STR_T,INT_T,VOID_T,NFLT_T,NINT_T,NSTR_T values of token_type_t.
+ */
+typedef int datatype_t;
+#endif
 
 /**
  * @brief Symbol structure data type.
- * 
+ *
  */
-typedef struct {
+typedef struct
+{
 	symbol_type_t symbol_type;
 
 	bool defined;
-
+		
 	bool variable_param_count;
+	
 	size_t param_count;
-	int* param_types_array;	
+	datatype_t *param_types_array;
 
-	int return_type;
+	datatype_t return_type;
 } symbol_info_t;
-
