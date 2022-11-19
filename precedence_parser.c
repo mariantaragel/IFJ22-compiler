6472640128fe8f13ed1217fd27bd43d3dbeae4f1
SuprisinglyBoring <david.klajbl@gmail.com>
2022-11-19 15:22:19 +0100

    Added comments to precedence parser

--- precedence_parser.c
+++ precedence_parser.c
@@ -7,12 +7,15 @@
 #include "error.h"
 
 error_codes_t reduce(pstack_t *pstack){
+	// token arrays of precedence reduction rule elements of handle at the top of precedence stack
 	token_array_t *ta1, *ta2, *ta3;
 	ta1 = ta2 = ta3 = NULL;
 
+	// precedence rule elements representing handle at the top of precedence stack
 	prec_rule_elem_t e1, e2, e3;
 	e1 = e2 = e3 = PREC_RULE_ELEM_NAN;
 
+	// get handle from precedence stack (handle is popped from stack in reversed)
 	bool too_many_prec_rule_elements = false;
 	if(pstack_is_top_handle_start(pstack) == false){
 		ta3 = pstack_pop(pstack, &e3);
@@ -21,20 +24,32 @@ error_codes_t reduce(pstack_t *pstack){
 			if(pstack_is_top_handle_start(pstack) == false){
 				ta1 = pstack_pop(pstack, &e1);
 				if(pstack_is_top_handle_start(pstack) == false){
+					// start of handle was not found after popping 3 elements from precedence stack (maximum rule elements == 3)
 					too_many_prec_rule_elements = true;
 				}
 			}
 			else{
-				ta1 = ta2; ta2 = ta3; ta3 = NULL;
+				// shift precedence rule elements to start at e1
+				//   e1   e2   e3        e1   e2   e3 
+				// |NULL| e2 | e3 | -> | e2 | e3 |NULL|
 				e1 = e2; e2 = e3; e3 = PREC_RULE_ELEM_NAN;
+
+				// shift token arrays to start at ta1
+				ta1 = ta2; ta2 = ta3; ta3 = NULL;
 			}
 		}
 		else{
-			ta1 = ta3; ta3 = NULL;
+			// shift precedence rule elements to start at e1
+			//   e1   e2   e3        e1   e2   e3
+			// |NULL|NULL| e3 | -> | e3 |NULL|NULL|
 			e1 = e3; e3 = PREC_RULE_ELEM_NAN;
+
+			// shift token arrays to start at ta1
+			ta1 = ta3; ta3 = NULL;
 		}
 	}
 
+	// if start of handle was not found, there were too many rule elements (maximum == 3), than expression has invalid syntax
 	if(too_many_prec_rule_elements == true){
 		token_array_free(ta1);
 		token_array_free(ta2);
@@ -42,12 +57,15 @@ error_codes_t reduce(pstack_t *pstack){
 		return SYNTAX_ERROR;
 	}
 
+	// remove handle start of top terminal
 	pstack_set_top_terminal_handle_start(pstack, false);
-
-	prec_rule_action_t rule_action = match_precedence_rule(e1, e2, e3);
 
 	error_codes_t res = OK;
 
+	// get precedence rule action based on precedence rule elements
+	prec_rule_action_t rule_action = match_precedence_rule(e1, e2, e3);
+
+	// apply precedence rule action
 	switch(rule_action){
 		case CONVERT_TO_EXPR:
 			if(pstack_push_nonterminal(pstack, ta1) == 0)
@@ -77,10 +95,12 @@ error_codes_t reduce(pstack_t *pstack){
 			res = SYNTAX_ERROR;
 	}
 
+	// free temporary token arrays
 	token_array_free(ta1);
 	token_array_free(ta2);
 	token_array_free(ta3);
-
+	
+	// return result (OK, SYNTAX_ERROR or INTERNAL_ERROR)
 	return res;
 }
 
@@ -104,18 +124,20 @@ token_array_t* parse_expression(token_array_t* expr_token_array){
 
 	error_codes_t prec_parser_error = OK;
 
-	prec_table_index_t top_term_prec_index;
-
 	token_t* input_token;
+	 
+	// indexes of precedence table
+	prec_table_index_t top_term_prec_index;
 	prec_table_index_t input_term_prec_index;
 
+	// operation from precedence table
 	prec_parser_operation_t prec_parser_operation;
 
 	bool done = false;
 
+	// get new input token
 	input_token = token_array_pop_token(expr_token_array);
 	
-
 	do{
 		// get precedence index of input token
 		input_term_prec_index = get_prec_table_index_of_token(input_token);
@@ -126,42 +148,42 @@ token_array_t* parse_expression(token_array_t* expr_token_array){
 		// get precedence parser operation from precedence table
 		prec_parser_operation = precedence_table_get_operation(top_term_prec_index, input_term_prec_index);
 
-		/*printf("------\n");
-		printf("[%d,%d]\n", top_term_prec_index, input_term_prec_index);
-		if(input_token != NULL){
-			t_print(input_token);
-		}
-		else{
-			printf("NULL\n");
-		}*/
-
 		// execute precedence parser operation
 		switch(prec_parser_operation){
 			case E:
-				//printf("E\n");
+				// push input token to precedence stack
 				if(pstack_push_terminal(pstack, input_token) != 0){
 					prec_parser_error = INTERNAL_ERROR;
 					done = true;
 				}
+
+				// get next input token
 				input_token = token_array_pop_token(expr_token_array);
 				break;
+
 			case S:
-				//printf("S\n");
+				// set handle start to true of tomost terminal
 				pstack_set_top_terminal_handle_start(pstack, true);
+
+				// push input token to precedence stack
 				if(pstack_push_terminal(pstack, input_token) != 0){
 					prec_parser_error = INTERNAL_ERROR;
 					done = true;
 				}
+
+				// get next input token
 				input_token = token_array_pop_token(expr_token_array);
 				break;
+
 			case R:
-				//printf("R\n");
+				// apply reduction rules
 				if((prec_parser_error = reduce(pstack)) != OK){
 					done = true;
 				}
 				break;
+
 			default:
-				//printf("X\n");
+				// syntax error occured
 				prec_parser_error = SYNTAX_ERROR;
 				done = true;
 		}
@@ -171,6 +193,7 @@ token_array_t* parse_expression(token_array_t* expr_token_array){
 			if(pstack_get_size(pstack) == 2){
 				if(pstack_get_top_terminal_prec_table_index(pstack) == DOLLAR_INDEX){
 					if(input_token == NULL){
+						// we are at the end of input and precedence stack contains only dollar symbol and nonterminal
 						done = true;
 					}
 				}
@@ -178,20 +201,28 @@ token_array_t* parse_expression(token_array_t* expr_token_array){
 		}
 	}while(!done);
 
+	// free last input token
 	if(input_token != NULL) t_dstr(input_token);
+
+	// free original token array
 	token_array_free(expr_token_array);
 
 	if(prec_parser_error != OK){
+		// set global error variable, free precedence stack and return NULL
+		error = prec_parser_error;
 		pstack_free(pstack);
-		error = SYNTAX_ERROR;
 		return NULL;
 	}
 	else{
+		// pop postfix reprezentation of expression token array from precedence stack
 		token_array_t* posfix_expr_token_array = pstack_pop(pstack, NULL);
 		if(posfix_expr_token_array == NULL){
 			error = INTERNAL_ERROR;
 		}
+		// free precedence stack
 		pstack_free(pstack);
+
+		// return posftix expression
 		return posfix_expr_token_array;
 	}
 }
--- precedence_stack.h
+++ precedence_stack.h
@@ -39,28 +39,68 @@ pstack_t * pstack_create();
  */
 void pstack_free(pstack_t *pstack);
 
-prec_table_index_t pstack_get_top_terminal_prec_table_index(pstack_t* pstack);
-
-void pstack_set_top_terminal_handle_start(pstack_t* pstack, bool handle_start);
-
-bool pstack_is_top_handle_start(pstack_t* pstack);
-
-
-int pstack_push_terminal(pstack_t* pstack, token_t* token);
-
-int pstack_push_nonterminal(pstack_t* pstack, token_array_t* token_array);
-
-
-
-token_array_t* pstack_pop(pstack_t* pstack, bool* is_terminal);
-
 /**
- * @brief Returns number of symbols stored in precedence stack.
+ * @brief Get precedence table index of topmost terminal of precedence stack.
  * 
  * @param pstack Precedence stack.
- * @return size_t Number of symbols in precedence stack.
+ * @return prec_table_index_t Returns precedence table index of topmost terminal on success, 
+ * othewise if topmost terminal doesnt exist ERROR_INDEX is returned.
+ */
+prec_table_index_t pstack_get_top_terminal_prec_table_index(pstack_t* pstack);
+
+/**
+ * @brief Sets handle start indicator of topmost terminal in precedence stack to specified value.
+ * If there are no terminals in precedence stack, nothing happens.
+ * Otherwise handle start indicator of topmost terminal is set to handle_start value.
+ * 
+ * @param pstack Precedence stack.
+ * @param handle_start Speicifies value to which topmost terminal handle start indicator will be set to.
+ */
+void pstack_set_top_terminal_handle_start(pstack_t* pstack, bool handle_start);
+
+/**
+ * @brief Checks whether top element of precedence stack has handle start set to true.
+ * 
+ * @param pstack Precedence stack.
+ * @return Returns true if top element of precedence stack has handle start indicator set to true, otherwise false is returned.
+ */
+bool pstack_is_top_handle_start(pstack_t* pstack);
+
+/**
+ * @brief Pushes token as terminal to precedence stack.
+ * Handle start indicator of newly added terminal is set to false.
+ * 
+ * @param pstack Precedence stack.
+ * @param token Token to be pushed in precedence stack.
+ * @return int Returns 0 on success, otherwise if allocation error occured, pstack is NULL or token is NULL non zero value is returned.
+ */
+int pstack_push_terminal(pstack_t* pstack, token_t* token);
+
+/**
+ * @brief Pushes token array as nonterminal to precedence stack.
+ * 
+ * @param pstack Precedence stack to which new nonterminal will be pushed.
+ * @param token_array Token array to be pushed in precedence stack.
+ * @return int Returns 0 on success, otherwise if allocation error occured non zero value is returned.
+ */
+int pstack_push_nonterminal(pstack_t* pstack, token_array_t* token_array);
+
+/**
+ * @brief Pops top element from precedence stack, returns token array representing postfix subexpression stored in popped element
+ * and stores information to precedence rule element structure, representing popped element as precedence rule element.
+ * 
+ * @param pstack Precedence stack from which top element will be popped.
+ * @param prec_rule_elem Precedence rule element structure where information about popped element will be stored.
+ * @return token_array_t* Returns token array representing postfix subexpression of popped element on success, otherwise NULL is returned.
+ */
+token_array_t* pstack_pop(pstack_t* pstack, prec_rule_elem_t* prec_rule_elem);
+
+/**
+ * @brief Returns number of elements stored in precedence stack.
+ * 
+ * @param pstack Precedence stack.
+ * @return size_t Number of elements in precedence stack.
  */
 size_t pstack_get_size(pstack_t* pstack);
-
 
 #endif
