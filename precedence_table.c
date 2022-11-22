4f16e596bad4b90c045777e3d1abda712f3020b2
SuprisinglyBoring <david.klajbl@gmail.com>
2022-11-22 22:03:26 +0100

    Changed precedence table to accept expressions with multiple relational operators

--- precedence_table.c
+++ precedence_table.c
@@ -11,8 +11,8 @@ const prec_parser_operation_t precedence_table[PRECEDENCE_TABLE_SIZE][PRECEDENCE
     //	        |+,-,.| *,/ | === | rel |  (  |  )  |  i  |  $  |
     /* +,-,. */ {  R  ,  S  ,  R  ,  R  ,  S  ,  R  ,  S  ,  R  }, 
     /*  *,/  */ {  R  ,  R  ,  R  ,  R  ,  S  ,  R  ,  S  ,  R  },
-    /*  ===  */ {  S  ,  S  ,  X  ,  S  ,  S  ,  R  ,  S  ,  R  }, 
-    /*  rel  */ {  S  ,  S  ,  R  ,  X  ,  S  ,  R  ,  S  ,  R  }, 
+    /*  ===  */ {  S  ,  S  ,  R  ,  S  ,  S  ,  R  ,  S  ,  R  }, 
+    /*  rel  */ {  S  ,  S  ,  R  ,  R  ,  S  ,  R  ,  S  ,  R  }, 
     /*   (   */ {  S  ,  S  ,  S  ,  S  ,  S  ,  E  ,  S  ,  X  },
     /*   )   */ {  R  ,  R  ,  R  ,  R  ,  X  ,  R  ,  X  ,  R  },
     /*   i   */ {  R  ,  R  ,  R  ,  R  ,  X  ,  R  ,  X  ,  R  },
